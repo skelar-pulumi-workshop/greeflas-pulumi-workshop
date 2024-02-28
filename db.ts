8badd016a0fe05cd2b820ae13d37c384db8d57dd
greeflas <vldmr.kuprienko@gmail.com>
2024-02-28 19:58:28 +0200

    Stage 2: custom component

--- db.ts
+++ db.ts
@@ -1,9 +1,18 @@
 import * as gcp from "@pulumi/gcp";
+import * as pulumi from "@pulumi/pulumi";
 
-export const db = new gcp.sql.DatabaseInstance("main", {
-    databaseVersion: "POSTGRES_15",
-    region: "us-central1",
-    settings: {
-        tier: "db-f1-micro",
-    },
-});
+export class ApplicationDB extends pulumi.ComponentResource {
+    public dbInstance: gcp.sql.DatabaseInstance
+
+    constructor(name: string, opts: pulumi.ComponentResourceOptions) {
+        super("pkg:index:ApplicationDB", name, {}, opts);
+
+        this.dbInstance = new gcp.sql.DatabaseInstance("main", {
+            databaseVersion: "POSTGRES_15",
+            region: "us-central1",
+            settings: {
+                tier: "db-f1-micro",
+            },
+        }, { parent: this, aliases: ["urn:pulumi:dev::base::gcp:sql/databaseInstance:DatabaseInstance::main"] });
+    }
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
-import { db } from "./db";
+import { ApplicationDB } from "./db";
 import { myCluster } from "./k8s";
 import { myInstance } from "./vm";
 
-export const dbPublicIP = db.publicIpAddress;
+const appDB = new ApplicationDB("my-db", {})
+
+export const dbPublicIP = appDB.dbInstance.publicIpAddress;
 export const clusterName = myCluster.name;
 export const instanceName = myInstance.name;
