4d12e6a30453b35adc3eea4373fd90682e9868c2
greeflas <vldmr.kuprienko@gmail.com>
2024-02-28 20:47:50 +0200

    Stage 2: create DB & user

--- db.ts
+++ db.ts
@@ -1,11 +1,19 @@
 import * as gcp from "@pulumi/gcp";
 import * as pulumi from "@pulumi/pulumi";
 
+export interface ApplicationDBArgs {
+    databaseVersion: string;
+    postgresPassword: string;
+    schemas: string[];
+}
+
 export class ApplicationDB extends pulumi.ComponentResource {
     public dbInstance: gcp.sql.DatabaseInstance
 
-    constructor(name: string, opts: pulumi.ComponentResourceOptions) {
+    constructor(name: string, args: ApplicationDBArgs, opts: pulumi.ComponentResourceOptions) {
         super("pkg:index:ApplicationDB", name, {}, opts);
+
+        const parentOpts = { parent: this, ...opts };
 
         this.dbInstance = new gcp.sql.DatabaseInstance("main", {
             databaseVersion: "POSTGRES_15",
@@ -13,6 +21,17 @@ export class ApplicationDB extends pulumi.ComponentResource {
             settings: {
                 tier: "db-f1-micro",
             },
-        }, { parent: this, aliases: ["urn:pulumi:dev::base::gcp:sql/databaseInstance:DatabaseInstance::main"] });
+        }, { aliases: ["urn:pulumi:dev::base::gcp:sql/databaseInstance:DatabaseInstance::main"], ...parentOpts });
+
+        const database = new gcp.sql.Database("app", {
+            name: "app",
+            instance: this.dbInstance.name,
+        }, parentOpts);
+
+        const postgresUser = new gcp.sql.User("postgres", {
+            name: "postgres",
+            instance: this.dbInstance.name,
+            password: args.postgresPassword,
+        }, parentOpts)
     }
 }
--- index.ts
+++ index.ts
@@ -1,8 +1,25 @@
 import { ApplicationDB } from "./db";
 import { myCluster } from "./k8s";
 import { myInstance } from "./vm";
+import * as pulumi from "@pulumi/pulumi";
 
-const appDB = new ApplicationDB("my-db", {})
+interface DBConfig {
+    databaseVersion: string;
+    postgresPassword: string;
+    schemas: string[];
+}
+
+const config = new pulumi.Config();
+const dbConfig = config.requireObject<DBConfig>("dbConfig")
+
+const appDB = new ApplicationDB("my-db", {
+    databaseVersion: "POSTGRES_15",
+    postgresPassword: dbConfig.postgresPassword,
+    schemas: [
+        "foo",
+        "bar",
+    ],
+}, {})
 
 export const dbPublicIP = appDB.dbInstance.publicIpAddress;
 export const clusterName = myCluster.name;
